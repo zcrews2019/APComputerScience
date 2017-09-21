244cd2d11611d95e6765195c0dd836930af90281
zcrews2019 <zcrews2019@cchsraider.net>
2017-09-21 13:53:06 -0400

    Commit projects  2-3 and 2-4

---
+++ Projects23and24/src/Project24.java
@@ -0,0 +1,15 @@
+
+// Project 2-4
+public class Project24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int minute = 1;
+		int minutesPerHour = minute * 60;
+		int minutesPerDay = minutesPerHour * 24;
+		int minutesPerYear = minutesPerDay * 365;
+		System.out.println("There are " + minutesPerYear + " minutes in a year!");
+
+	}
+
+}
